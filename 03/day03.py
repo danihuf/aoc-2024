58bd44387c9354ea4a49ac40e0bf06495fc6aa40
danihuf <daniel99h@gmail.com>
2024-12-04 00:53:16 +0100

    add day 3 part 1

---
+++ 03/day03.py
@@ -0,0 +1,32 @@
+# read input file
+import sys
+import re
+
+input = '/Users/daniel/Documents/code/advent-of-code/aoc-2024/03/input.txt'
+
+def parseInput(path: str) -> str:
+    lines: str = ""
+    with open(path, "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()
+            lines += line
+
+    return lines
+
+
+def calculateValidMemory(memory: str, regex: str) -> int:
+    count = 0
+    validMem = re.findall(regex, memory, re.MULTILINE)
+    for mem in validMem:
+        nums = [int(n) for n in mem.split(',')]
+        count += (nums[0] * nums[1])
+    return count
+
+
+def part1():
+    corruptedMem = parseInput(input)
+    # matches all valid multiplication 
+    regex = r'mul\((\d{1,3},\d{1,3})\)'
+    print(calculateValidMemory(corruptedMem, regex))
+
+part1()
